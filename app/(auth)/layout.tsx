35789f61810e5ac621dcf5724db5d85954c67f7c
Deepak <deekay9357@gmail.com>
2023-09-03 15:44:00 +0530

    projectD@1.0.2 BarsLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata : Metadata = {
-    title: "Auth",
+    title: "Project D",
     description: "A Next.js Project",
   };
  
