62ca80e59aa4cbeb752535de64ab9c16ae09bd9f
Deepak <deekay9357@gmail.com>
2023-09-05 00:19:39 +0530

    projectD@1.0.7 Comment

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 
 import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
+import Comment from "@/components/forms/Comment";
 
 const Page = async({ params }: { params: { id: string}}) =>{
     if (!params.id) return null;
@@ -18,18 +19,43 @@ const Page = async({ params }: { params: { id: string}}) =>{
 
     return(
         <section className='relative'>
-        <div>
-            <ThreadCard
-            id={thread._id}
-            currentUserId={user.id}
-            parentId={thread.parentId}
-            content={thread.text}
-            author={thread.author}
-            community={thread.community}
-            createdAt={thread.createdAt}
-            comments={thread.children}
-            />
-        </div>
+            <div>
+                <ThreadCard
+                id={thread._id}
+                currentUserId={user.id}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+                />
+            </div>
+
+            <div className='mt-7'>
+                <Comment
+                threadId={params.id}
+                currentUserImg={user.imageUrl} //userInfo.image
+                currentUserId={JSON.stringify(userInfo._id)}
+                />
+            </div>
+
+            <div className='mt-10'>
+                {thread.children.map((childItem: any) => (
+                <ThreadCard
+                    key={childItem._id}
+                    id={childItem._id}
+                    currentUserId={user.id}
+                    parentId={childItem.parentId}
+                    content={childItem.text}
+                    author={childItem.author}
+                    community={childItem.community}
+                    createdAt={childItem.createdAt}
+                    comments={childItem.children}
+                    isComment
+                />
+                ))}
+            </div>
         </section>
     )
 }
